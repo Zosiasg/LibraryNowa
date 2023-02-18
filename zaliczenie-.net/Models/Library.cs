7467ecd87a0fa09408e0783b21517f1530391288
Zofia Gerstenkorn <zosiasg@gmail.com>
2023-02-18 16:28:27 +0100

    dodana walidacja do pól na tabeli library

--- zaliczenie-.net/Models/Library.cs
+++ zaliczenie-.net/Models/Library.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace zaliczenie_.net.Models
 {
     public class Library
     {
         public int idBook { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [RegularExpression(@"^[A-ZŁŚ]\w.*", ErrorMessage = "The field Tittle must start with capital letter")]
         public string Tittle { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [RegularExpression(@"^[A-ZŁŚ]\w.*", ErrorMessage = "The field Author must start with capital letter")]
         public string Author { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Section { get; set; }
+        [Required(ErrorMessage = "The Status field is required.")]
         public Status Status { get; set; }
     }
     public enum Status
